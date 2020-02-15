75144b88cb0a151eb3dabc02d354393e6843bf30
windygoah <1102345426@qq.com>
2020-02-15 20:19:50 +0800

    first commit

---
+++ src/test/java/club/lizike/community/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package club.lizike.community.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
